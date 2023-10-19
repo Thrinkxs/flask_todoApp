2813053bea7f311aeae199753307868ec433ef4b
Thrinkxs <emmanuelozigue@gmail.com>
2023-10-19 16:06:34 +0200

    feature-added-integration-test

--- backend/integration.py
+++ backend/integration.py
@@ -11,28 +11,35 @@ class TestTodoRoutes(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client()
 
-        # Set up a test database
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mkavnntb:Lvxr4i1weHxWW5wMRyHoCoPyXPwkfL5J@rain.db.elephantsql.com/mkavnntb'
+
+        self.app.config['TESTING'] = True
+        self.app.config['SERVER_NAME'] = 'localhost'
+
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
         with self.app.app_context():
             db.create_all()
+        self.todo = Todo(task="Test Task", completed=False)
+        db.session.add(self.todo)
+        db.session.commit()
 
     def tearDown(self):
-        with self.app.app_context():
-            db.session.remove()
-            db.drop_all()
+        self.app_context.pop()
 
     def test_add_and_delete_todo(self):
 
         response = self.client.post(
             url_for('blueprint.add_todo'), data={'task': 'Test Task'})
         self.assertEqual(response.status_code, 302)
-        todo = Todo.query.filter_by(task='Test Task').first()
+        todo = Todo.query.session.get(Todo, self.todo.id)
         self.assertIsNotNone(todo)
 
         response = self.client.get(
             url_for('blueprint.delete', todo_id=todo.id))
         self.assertEqual(response.status_code, 302)
-        todo = Todo.query.filter_by(task='Test Task').first()
+        todo = Todo.query.session.get(Todo, self.todo.id)
         self.assertIsNone(todo)
 
 
