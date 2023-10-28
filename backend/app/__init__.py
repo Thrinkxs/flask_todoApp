5f6b6e3d76696895d7db925bc382246701d0db7d
Thrinkxs <66226531+Thrinkxs@users.noreply.github.com>
2023-10-28 12:50:32 +0200

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,8 +9,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, template_folder='../../frontend/templates',
                 static_folder='../../frontend/static')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mkavnntb:Lvxr4i1weHxWW5wMRyHoCoPyXPwkfL5J@rain.db.elephantsql.com/mkavnntb'
-    database_url = 'postgresql://mkavnntb:Lvxr4i1weHxWW5wMRyHoCoPyXPwkfL5J@rain.db.elephantsql.com/mkavnntb'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = ""
+    database_url = ""
     app.config.update(
         SQLALCHEMY_DATABASE_URI=database_url,
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
